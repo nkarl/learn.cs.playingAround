5f782c970d4d5a920ca4efc55370bb4efd8effab
Karl N <nordenk@pm.me>
2021-06-08 23:29:36 -0700

    updated

--- PlayingAround/Program.cs
+++ PlayingAround/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace PlayingAround
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string greeting = "Hello, World!";
+            Console.WriteLine(greeting);
+
+            // This is an example of ArrayList, it's seems flexible and can be used on the fly for any data types.
+            var array = new List<string> {greeting};
+            Console.WriteLine(array[0]);
+            Console.WriteLine(greeting.GetType());
+            Console.WriteLine((array[0])?.GetType());
+            Console.WriteLine($"content of <greeting> is equal to content of <array[0]>: {greeting.Equals(array[0])}");
+            Console.WriteLine(
+                $"Type of <greeting, {greeting.GetType()}> is same as type of <array[0], {array[0]?.GetType()}>: {(greeting.GetType() == array[0]?.GetType())}");
         }
     }
 }
